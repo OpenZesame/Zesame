57182458271061209d6b1be01fe433a933ddd791
Sajjon <alex.cyon@gmail.com>
2018-09-20 22:59:03 +0200

    clean up of SDK

--- Source/Services/DefaultZilliqaService.swift
+++ Source/Services/DefaultZilliqaService.swift
@@ -12,26 +12,16 @@ import JSONRPCKit
 import APIKit
 
 public typealias ZilliqaServiceReactive = ZilliqaService & ReactiveCompatible
+
 public final class DefaultZilliqaService: ZilliqaServiceReactive {
 
     public let wallet: Wallet
 
-    public let apiClient: APIClient = JsonRpcClient()
+    public let apiClient: APIClient = DefaultAPIClient()
 
     public init(wallet: Wallet) {
         self.wallet = wallet
     }
-}
-
-public protocol APIClient {
-    func send<Request, Response>(request: Request, done: @escaping RequestDone<Response>) where Request: JSONRPCKit.Request, Request.Response == Response
-}
-
-public final class JsonRpcClient: APIClient {
-
-    private let batchFactory = BatchFactory(version: "2.0", idGenerator: NumberIdGenerator())
-
-    public init() {}
 }
 
 public extension DefaultZilliqaService {
@@ -42,24 +32,5 @@ public extension DefaultZilliqaService {
 
     func send(transaction: Transaction, done: @escaping RequestDone<TransactionIdentifier>) {
         return apiClient.send(request: TransactionRequest(transaction: transaction), done: done)
-    }
-}
-
-// MARK: - DefaultZilliqaService APIKit
-public extension JsonRpcClient {
-    func send<Request, Response>(request: Request, done: @escaping RequestDone<Response>) where Request: JSONRPCKit.Request, Request.Response == Response {
-        let batch = batchFactory.create(request)
-        let httpRequest = ZilliqaRequest(batch: batch)
-        let handlerAPIKit = mapHandler(done)
-
-        let _: SessionTask? = Session.send(httpRequest, callbackQueue: nil) { result in
-            switch result {
-            case .success(let response):
-                handlerAPIKit(.success(response))
-            case .failure(let error):
-                print("⚠️ \(error)")
-                handlerAPIKit(.failure(error))
-            }
-        }
     }
 }
--- Source/Services/ZilliqaService.swift
+++ Source/Services/ZilliqaService.swift
@@ -15,15 +15,19 @@ import RxCocoa
 import RxSwift
 
 public typealias TransactionIdentifier = String
+
 public protocol ZilliqaService {
+
     var wallet: Wallet { get }
     var apiClient: APIClient { get }
+
     func getBalalance(done: @escaping RequestDone<BalanceResponse>)
 
     func send(transaction: Transaction, done: @escaping RequestDone<TransactionIdentifier>)
 }
 
 public extension ZilliqaService {
+    
     func signAndMakeTransaction(payment: Payment, using keyPair: KeyPair, done: @escaping RequestDone<TransactionIdentifier>) {
         let transaction = sign(payment: payment, using: keyPair)
         send(transaction: transaction, done: done)
@@ -45,61 +49,4 @@ public extension ZilliqaService {
     func sign(message: Message, using keyPair: KeyPair) -> Signature {
         return Signer.sign(message, using: keyPair)
     }
-}
-
-extension String {
-    enum Case {
-        case lower, upper
-    }
-    func `case`(_ `case`: Case) -> String {
-        switch `case` {
-        case .lower: return lowercased()
-        case .upper: return uppercased()
-        }
-    }
-}
-
-func messageFromUnsignedTransaction(_ tx: UnsignedTransaction, publicKey: PublicKey) -> Message {
-        func hex64B(_ number: BigNumber) -> String {
-            let hex = number.asHexStringLength64()//.case(`case`)
-            assert(hex.count == 64)
-            return hex
-        }
-        func hex64D(_ number: Double) -> String {
-            return hex64B(BigNumber(number))
-        }
-        func hex64I(_ number: Int) -> String {
-            return hex64D(Double(number))
-        }
-
-    func eightChar(from int: Int) -> String {
-        return String(format: "%08d", int)
-    }
-
-    func formatCodeOrData(from string: String) -> String {
-        if let data = string.data(using: .utf8) {
-            return data.toHexString()
-        } else {
-            print("Failed to create Swift.Data from `code` or `data` having value: \n`\(string)`")
-            return ""
-        }
-    }
-
-    let codeHex = formatCodeOrData(from: tx.code)
-    let dataHex = formatCodeOrData(from: tx.data)
-
-        let hexString = [
-            hex64I(tx.version),
-            hex64I(tx.nonce),
-            tx.to.case(.upper),
-            publicKey.hex.compressed.case(.lower),
-            hex64D(tx.amount).case(.lower),
-            hex64D(tx.gasPrice),
-            hex64D(tx.gasLimit),
-            eightChar(from: tx.code.count),
-            codeHex,
-            eightChar(from: tx.data.count),
-            dataHex,
-            ].joined()
-    return Message(hex: hexString)
 }
