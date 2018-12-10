7ccdc133d66e3db7dfe574aacc599666fe318bf1
Sajjon <alex.cyon@gmail.com>
2018-12-10 22:09:23 +0700

    simplifying formatting

--- Source/Models/Manual/Payment/UnsignedTransaction.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Transaction.swift
-//  Zesame-iOS
-//
-//  Created by Alexander Cyon on 2018-10-07.
-//  Copyright © 2018 Open Zesame. All rights reserved.
-//
-
-import Foundation
--- Source/Services/APIModels/Transaction/MessageFromUnsignedTransaction.swift
+++ Source/Services/APIModels/Transaction/MessageFromUnsignedTransaction.swift
@@ -56,68 +56,9 @@ func messageFromUnsignedTransaction(_ tx: Transaction, publicKey: PublicKey, has
 }
 
 
-public struct SignedTransaction {
 
-    private let signature: String
-    private let transaction: Transaction
-    private let publicKeyCompressed: String
 
-    init(transaction: Transaction, signedBy publicKey: PublicKey, signature: Signature) {
-        self.transaction = transaction
-        self.publicKeyCompressed = publicKey.hex.compressed
-        self.signature = signature.asHexString()
-    }
-}
 
-extension SignedTransaction: Encodable {
-
-    enum CodingKeys: String, CodingKey {
-        case version, toAddr, nonce, pubKey, amount, gasPrice, gasLimit, code, data, signature
-    }
-
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-
-        let tx = transaction
-        let p = tx.payment
-
-        try container.encode(tx.version, forKey: .version)
-        try container.encode(p.nonce.nonce, forKey: .nonce)
-        try container.encode(p.recipient, forKey: .toAddr)
-        try container.encode(publicKeyCompressed, forKey: .pubKey)
-
-        let k = CodingKeys.self
-        try zip(
-            [p.amount, p.gasPrice, p.gasLimit],
-            [k.amount, k.gasPrice, k.gasLimit]
-        ).forEach { (value, key) in
-            try container.encode(value.asUInt64.description, forKey: key)
-        }
-
-//        try container.encode(p.amount.asUInt64, forKey: .amount)
-//        try container.encode(p.gasPrice.asUInt64, forKey: .gasPrice)
-//        try container.encode(p.gasLimit.asUInt64, forKey: .gasLimit)
-
-        try container.encode(tx.code, forKey: .code)
-        try container.encode(tx.data, forKey: .data)
-        try container.encode(signature, forKey: .signature)
-
-    }
-}
-
-public struct Transaction {
-    let version: UInt32
-    let payment: Payment
-    let data: String
-    let code: String
-
-    init(payment: Payment, version: UInt32 = 1, data: String = "", code: String = "") {
-        self.version = version
-        self.payment = payment
-        self.data = data
-        self.code = code
-    }
-}
 
 // MARK: - Private format helpers
 private extension Amount {
