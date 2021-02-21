99cb6025c82adec8a8df5998b130ce16a29e22ce
Alexander Cyon <alex.cyon@gmail.com>
2021-02-21 22:12:42 +0100

    Bump iOS target to 13

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Zesame",
-    platforms: [.macOS(.v10_15), .iOS(.v12)],
+    platforms: [.macOS(.v10_15), .iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
