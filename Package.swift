f64266d72219fc67369841593dfe144dfb3b3bb8
Alexander Cyon <alex.cyon@gmail.com>
2022-02-13 11:54:10 +0100

    Remove Alamofire from Package.resolved since it is not used anymore.

--- Package.swift
+++ Package.swift
@@ -16,15 +16,14 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/Sajjon/EllipticCurveKit.git", .upToNextMinor(from: "1.0.2")),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMinor(from: "1.4.3")),
-        .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.2.0")),
+        .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Zesame",
-            dependencies: ["EllipticCurveKit", "CryptoSwift", "SwiftProtobuf", "Alamofire"],
+            dependencies: ["EllipticCurveKit", "CryptoSwift", "SwiftProtobuf"],
             exclude: ["Models/Protobuf/messages.proto"]
         ),
         .testTarget(
